acc9c5dd3c2cf3e21b525d609158a799c3b07ec1
olemoreflipflop <fil.olga.ev@gmail.com>
2022-05-14 20:30:48 +0300

    added all files of project

---
+++ src/test/java/com/olemore/FailedTests.java
@@ -0,0 +1,28 @@
+package com.olemore;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FailedTests {
+
+    @Test
+    void test00(){
+        assertTrue(false);
+    }
+
+    @Test
+    void test01(){
+        assertTrue(false);
+    }
+
+    @Test
+    void test02(){
+        assertTrue(false);
+    }
+
+    @Test
+    void test03(){
+        assertTrue(false);
+    }
+}
