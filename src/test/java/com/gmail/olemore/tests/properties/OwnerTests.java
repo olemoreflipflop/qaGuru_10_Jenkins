bfdb3bd0b907985ba8f231e16d8f19e036790928
olemoreflipflop <fil.olga.ev@gmail.com>
2022-05-17 14:58:31 +0300

    added owner example

---
+++ src/test/java/com/gmail/olemore/tests/properties/OwnerTests.java
@@ -0,0 +1,23 @@
+package com.gmail.olemore.tests.properties;
+
+import com.gmail.olemore.config.CredentialsConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class OwnerTests {
+    CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+
+    @Test
+    @Tag("owner")
+    void loginTest() {
+        String login = config.login();
+        String password = config.password();
+
+        System.out.println("Login: " + login);
+        System.out.println("Password: " + password);
+
+        String message = "I logged in as " + login + " with password " + password;
+        System.out.println(message);
+    }
+}
