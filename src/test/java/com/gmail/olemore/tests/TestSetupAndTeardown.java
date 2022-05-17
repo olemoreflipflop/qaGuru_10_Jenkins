7d5b487f45a0c2dcabee4a6a2e0f5ca9048afa1a
olemoreflipflop <fil.olga.ev@gmail.com>
2022-05-17 16:33:29 +0300

    added selenoid url and login+password config option

--- src/test/java/com/gmail/olemore/tests/TestSetupAndTeardown.java
+++ src/test/java/com/gmail/olemore/tests/TestSetupAndTeardown.java
@@ -2,8 +2,10 @@ package com.gmail.olemore.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import com.gmail.olemore.config.CredentialsConfig;
 import com.gmail.olemore.testHelpers.AllureAttachments;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -12,13 +14,23 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestSetupAndTeardown {
 
+    static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+    static String login = config.login();
+    static String password = config.password();
+    static String selenoidUrl = System.getProperty("selenoidUrl", "selenoid.autotests.cloud/wd/hub");
+    static String selenoidLogin = "https://" + login + ":" + password + "@" + selenoidUrl;
+
     @BeforeAll
     static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
+        String browser = System.getProperty("browser", "chrome");
+        String browserSize = System.getProperty("browserSize");
+
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1280x1400";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browser = browser;
+        Configuration.browserSize = browserSize;
+        Configuration.remote = selenoidLogin;
 
         //Добавляем видео в отчет
         DesiredCapabilities capabilities = new DesiredCapabilities();
